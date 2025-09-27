29ac994522d68018cb33c35f59109be92621270d
NachiketKandari <nachiket.kandari@gmail.com>
2025-09-28 02:34:49 +0530

    feat: added fps

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    dt = 0 # delta time
 
     while True: # Loop for the screen
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000 # milliseconds to seconds
+
 
 
 if __name__ == "__main__":
